edfbec3b628292116a08438f14917a53aafea660
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-12 18:14:05 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('../pages/Home/Home'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
+
+export default App;
+
+// export const App = () => {
+//   return (
+//     <div
+//     // style={{
+//     //   height: '100vh',
+//     //   display: 'flex',
+//     //   justifyContent: 'center',
+//     //   alignItems: 'center',
+//     //   fontSize: 40,
+//     //   color: '#010101'
+//     // }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
