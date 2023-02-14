17800e5a7ab9b2eba3843cb23606023f26cc4147
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-13 20:56:17 -0500

    update

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -4,7 +4,7 @@ export const fetchMovie = async (path, query, page) => {
   const response = await axios.get(`https://api.themoviedb.org/3/${path}`, {
     method: 'get',
     params: {
-      api_key: '33085035-b16638eb1fe5c5711fce4390a',
+      api_key: 'c69fb8daa91af9db5bc79adaff004050',
       query: query,
       page: page,
     },
