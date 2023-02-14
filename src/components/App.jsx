0480ffc44f01b2adab3e8d0044e553d6fac44d5d
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-13 20:58:40 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
+import { SharedLayout } from './SharedLayouts/SharedLayouts';
 
 const Home = lazy(() => import('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() =>
+  import('../pages/Movies/MovieDetails/MovieDetails')
+);
+const Cast = lazy(() => import('../pages/Movies/Cast/Cast'));
+const Reviews = lazy(() => import('../pages/Movies/Reviews/Reviews'));
 
-const App = () => {
+export const App = () => {
   return (
     <>
       <Routes>
-        <Route>
+        <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </>
   );
 };
 
-export default App;
+// export default App;
 
 // export const App = () => {
 //   return (
